1defb5dfd1e7d34c4c0bd4d3727dd05ca8f4ad9d
Mert Ali Bayram <mertali.bayram01@dogaokullari.k12.tr>
2024-04-04 18:18:20 +0300

    update

--- CuffedOppositeFaction-Exiled8/Plugin.cs
+++ CuffedOppositeFaction-Exiled8/Plugin.cs
@@ -16,7 +16,7 @@ namespace CuffedOppositeFaction_Exiled8
         public override string Author { get; } = "6hundred9";
         public override string Name { get; } = "Cuffed Opposite Faction";
         public override string Prefix { get; } = "COF";
-        public override Version RequiredExiledVersion { get; } = new Version(8, 8, 1);
+        public override Version RequiredExiledVersion { get; } = new Version(8, 8, 0);
 
         public override void OnEnabled()
         {
